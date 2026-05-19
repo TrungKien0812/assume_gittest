7a112978ac460be272599fea1b4c0394ad047242
anhaylientuc <thang3051998no1@gmail.com>
2026-05-19 15:54:27 +0700

    chinh sua main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { ThangComponent } from "./components/ThangCompnent.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThangComponent />
   </StrictMode>,
 );
